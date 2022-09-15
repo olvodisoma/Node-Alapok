166ddbe2badc463b249acc65adf1a4257cc34fdf
MyNameIsDevid <hdevid12@gmail.com>
2022-09-15 13:10:59 +0200

    csutortokbackend

--- index.js
+++ index.js
@@ -15,4 +15,34 @@ app.get('/',(request,response) => {
     })
 })
 
+app.get('/author/:author',(request,response) => {
+    const {author} = request.params
+    db.query('SELECT title FROM books WHERE author=? ORDER BY title',[author],(error,result)=>{
+        if(error)
+            console.log("Hiba")
+        else
+            response.send(result)
+    })
+})
+
+app.get('/categ/:categ/year/:year',(request,response) => {
+    const {categ,year} = request.params
+    db.query('SELECT author,title,year FROM books WHERE category=? AND year>=?',[categ,year],(error,result)=>{
+        if(error)
+            console.log("Hiba")
+        else
+            response.send(result)
+    })
+})
+
+app.get('/id/:id',(request,response) => {
+    const {id} = request.params
+    db.query('SELECT author,title,year,category FROM books WHERE id=?',[id],(error,result)=>{
+        if(error)
+            console.log("Hiba")
+        else
+            response.send(result)
+    })
+})
+
 app.listen(5000,() => console.log('server listening on port'))
