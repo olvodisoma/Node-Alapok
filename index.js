232d9c332363021a2d90e587ce0bd4feeafa14fa
MyNameIsDevid <hdevid12@gmail.com>
2022-09-16 09:27:49 +0200

    pentek

--- index.js
+++ index.js
@@ -45,4 +45,64 @@ app.get('/id/:id',(request,response) => {
     })
 })
 
+app.put('/:id/:year',(request,response)=>{
+    const {id,year} = request.params
+    db.query('UPDATE books SET year=? WHERE id=?',[year,id],(error,result)=>{
+    if(error)
+        console.log("Hiba")
+    if(result.affectedRows == 1)
+        response.send({message:"Sikeres adatmódosítás."})
+    else
+        response.send({message:"Sikerestelen adatmódosítás."})
+    })
+})
+
+app.put('/',(request,response)=>{
+    const {id,category,year} = request.body
+    db.query('UPDATE books SET category=?, year=? WHERE id=?',[category,year,id],(error,result)=>{
+    if(error)
+        console.log("Hiba")
+    if(result.affectedRows!=0)
+        response.send({message:"Sikeres adatmódosítás."})
+    else
+        response.send({message:"Sikertelen adatmódosítás."})
+    })
+})
+
+app.post('/',(request,response)=>{
+    const {category,year,title,author} = request.body
+    db.query('INSERT INTO books VALUES (null,?,?,?,?);',[author,title,year,category],(error,result)=>{
+    if(error)
+        console.log("Hiba")
+    if(result.insertId)
+        response.send({message:`Sikeres adatmódosítás. id:${result.insertId}`})
+    else
+        response.send({message:"Sikertelen adatmódosítás."})
+    })
+})
+
+app.delete('/:id',(request,response)=>{
+    const {id} = request.params
+    db.query('DELETE FROM books WHERE id=?',[id],(error,result)=>{
+    if(error)
+        console.log("Hiba")
+    if(result.affectedRows!=0)
+        response.send({message:`Sikeres törlés.`})
+    else
+        response.send({message:"Sikertelen törlés."})
+    })
+})
+
+//A felhasználó látni szeretné hogy a kiválasztott kategóriában milyen könyvek vannak(szerző,cím)
+
+app.get('/category/:category',(request,response)=>{
+    const {category} = request.params
+    db.query('SELECT author,title FROM books WHERE category=?',[category],(error,result)=>{
+    if(error)
+        console.log("Hiba")
+    else
+        response.send(result)
+    })
+})
+
 app.listen(5000,() => console.log('server listening on port'))
